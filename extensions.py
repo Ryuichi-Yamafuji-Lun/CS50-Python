4d23a2b95f99a50b1b4c714f777f6dd2b6d14b9b
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-10-24 13:24:08 +0900

    implement exercise
    match cannot be implemented due to VS Code not having newest pylance

--- extensions.py
+++ extensions.py
@@ -1,1 +1,26 @@
 #implementation of the extension exercise
+def main():
+    File = input("File name: ")
+    extension = File.split(".")
+    if len(extension) != 2:
+        print("application/octet-stream")
+        return 0
+    #get extension
+    ext = extension[1]
+    if ext == "gif":
+        print("image/gif")
+    elif ext == "jpg":
+        print("image/jpg")
+    elif ext == "jpeg":
+        print("image/jpeg")
+    elif ext == ("png"):
+        print("image/png")
+    elif ext == "pdf":
+        print("image/pdf")
+    elif ext == "txt":
+        print("image/txt")
+    elif ext == "zip":
+        print("image/zip")
+    else:
+        print("application/octet-stream")
+main()
