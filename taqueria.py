49a8e5c9ace33c1a8bd0c73e4528b399a5dc8ba8
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-11-01 16:59:25 +0900

    Update taqueria.py

--- taqueria.py
+++ taqueria.py
@@ -1,1 +1,24 @@
 #implement taqueria exercise
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def main():
+    while(True):
+        try:
+            order = input("Item: ").title()
+            if (order == ""):
+                exit()
+            else:
+                print(f"Total: ${menu[order]:.2f}")
+        except KeyError:
+            pass
+main()
