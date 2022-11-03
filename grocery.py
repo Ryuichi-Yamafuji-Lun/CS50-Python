2aea7f45031fff3f16aa459fae69ddee6c4888ab
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-11-03 15:38:11 +0900

    implement grocery

--- grocery.py
+++ grocery.py
@@ -1,1 +1,21 @@
 #implementation of grocery exercise
+def main():
+    #list for the grocery
+    list = {}
+    while(True):
+        try:
+            #input and capitilize all
+            grocery = input().upper()
+            #check if grocery is in list
+            if grocery in list:
+                list[grocery] += 1
+            #if not then add the grocery
+            else:
+                list[grocery] = 1
+        #control-d to print out values
+        except EOFError:
+            #itterate through key and value of list items an print. 
+            for key, value in list.items():
+                print(f"{value} {key}")
+            exit()
+main()
