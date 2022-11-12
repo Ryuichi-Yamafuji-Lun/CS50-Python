9089acdc7cc2f1e85eab027eb540511ce214586a
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-11-13 00:12:36 +0900

    Update game.py

--- game.py
+++ game.py
@@ -3,20 +3,20 @@ from random import randint
 
 def main():
     while(True):
-        level = int(input("Level: "))
-        if level > 0:
-            random_num = randint(1,level)
-            while(True):
-                guess = int(input("Guess: "))
-                if guess == random_num:
-                    print("Just right!")
-                    exit()
-                elif guess < random_num and guess > 0:
-                    print("Too small!")
-                elif guess > random_num:
-                    print("Too large!")
-                else:
-                    pass
-        else: 
-            pass
+        level = input("Level: ")
+        while level.isnumeric() == False or int(level) <= 0:
+            level = input("Level: ")
+        random_num = randint(1,int(level))
+        while(True):
+            guess = input("Guess: ")
+            while guess.isnumeric() == False or int(guess) <= 0:
+                guess = input("Guess: ")
+            guess = int(guess)
+            if guess == random_num:
+                print("Just right!")
+                exit()
+            elif guess < random_num:
+                print("Too small!")
+            else:
+                print("Too large!")
 main()
