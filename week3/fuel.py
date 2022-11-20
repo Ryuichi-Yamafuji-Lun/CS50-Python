a7aa4ef358e0a3d22e489ac43556b440dd9826f4
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-11-20 12:49:40 +0900

    Update fuel.py

--- week3/fuel.py
+++ week3/fuel.py
@@ -1,30 +1,40 @@
 #implementation of fuel exercise
 def main():
-    x = 1
-    while x != 0:
-        fraction =  input("Fraction: ")
-        fraction = fraction.split("/")
-        x = calc(fraction[0], fraction[1])
+    while True:
+        try:
+            fraction =  input("Fraction: ")
+            percent = convert(fraction)
+            print(gauge(percent))
+        except (ValueError, ZeroDivisionError):
+            pass
 
-def calc(num, den):
+def convert(fraction):
+    fraction = fraction.split("/")
+    num = fraction[0]
+    den = fraction[1]
     if num.isnumeric() == True and den.isnumeric() == True:
         den = int(den)
         num = int(num)
         if den != 0 and num <= den:
-            percent = (num/den) * 100
-            if percent >= 99:
-                print("F")
-            elif percent == 0:
-                print("E")
-            else:
-                print(f"{percent}%")
-            return 0
+            percent = int((num/den) * 100)
+            return percent
+        elif num > den:
+            raise ValueError
         else:
-            return 1
+            raise ZeroDivisionError
     else:
-        return 1
+        raise ValueError
+    
+def gauge(percent):
+    percent = int(percent)
+    if percent >= 99:
+        return "F"
+    elif percent == 0:
+        return "E"
+    else:
+        return f"{percent}%"
 
 
 
-
-main()
+if __name__ == "__main__":
+    main()
