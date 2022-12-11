7204992b6feab0bbed85736fbb9f101643f945b2
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-12-11 18:00:45 +0900

    Update numb3rs.py

--- numb3rs.py
+++ numb3rs.py
@@ -6,3 +6,10 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
+    if re.search(r"^[0-255]+\.[0-255]+\.[0-255]+\.[0-255]",ip):
+        return True
+    else:
+        return False
+    
+if __name__ == "__main__":
+    main() 
