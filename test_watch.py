291c75ed21666720fc5049b5bb7f8d11053432c6
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-12-14 15:34:07 +0900

    create watch and test for watch exercise

---
+++ test_watch.py
@@ -0,0 +1,7 @@
+#test for watch.py
+from watch import parse
+
+def test_parse():
+    assert parse('<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>') == "https://www.youtu.be/xvFZjo5PgG0"
+    assert parse('<iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>') == "https://www.youtu.be/xvFZjo5PgG0"
+    
---
+++ watch.py
@@ -0,0 +1,11 @@
+#implementation of watch youtube exercise
+import re
+
+def main():
+    print(parse(input("HTML: ")))
+
+def parse(html):
+    
+
+if __name__ == "__main__":
+        main()
