28f037ee1f473f3276e240b6e26b5bf35c261506
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-12-18 20:48:42 +0900

    implement watch.py

--- watch.py
+++ watch.py
@@ -5,7 +5,9 @@ def main():
     print(parse(input("HTML: ")))
 
 def parse(html):
-    
+    html = re.search(r'src="(.*?)"', html)
+    html = "https://youtu.be/"+ html[0].rsplit('/',1)[1].replace('"','')
+    return html
 
 if __name__ == "__main__":
         main()
