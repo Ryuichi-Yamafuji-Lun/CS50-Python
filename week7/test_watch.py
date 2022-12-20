a88afd59a0651637d4eb6b2af1470083147a8b5c
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-12-20 18:51:36 +0900

    Update test_watch.py

--- week7/test_watch.py
+++ week7/test_watch.py
@@ -1,5 +1,5 @@
 #test for watch.py
-from week7.watch import parse
+from watch import parse
 
 def test_parse():
     assert parse('<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>') == "https://youtu.be/xvFZjo5PgG0"
