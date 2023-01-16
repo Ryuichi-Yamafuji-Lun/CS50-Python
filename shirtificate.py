4b50436bac943dd704ec6f7e0228e9a4ea57ce00
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-01-16 13:05:33 +0900

    Update shirtificate.py

--- shirtificate.py
+++ shirtificate.py
@@ -1,1 +1,14 @@
 #implementation of cs50 shirtificate exercise
+from fpdf import FPDF
+
+class Shirt(FPDF):
+    def __init__(self):
+        ...
+
+
+
+def main():
+    name = input("Name: ")
+
+if __name__ == "__main__":
+    main()
