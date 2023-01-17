7fc4652ea906a09d204c4c4609f040d2d3b813cb
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-01-17 20:26:54 +0900

    update shirtificate.py

--- shirtificate.py
+++ shirtificate.py
@@ -4,17 +4,18 @@ from fpdf import FPDF
 class Shirt(FPDF):
     def Page(self):
         #Setting Title of PDF
-        self.set_font('Times', 'U', 16)
-        
+        self.set_font('Times', 'B', 20)
+        self.cell(80)
+        self.cell(30, 10, "CS50 Shirtificate", border = 1, align= "C")
+        self.ln(20)
         #Render shirt png
-
-        
-
-
-
+        self.image(".../shirtificate.jpeg", 80)
 
 def main():
     name = input("Name: ")
+    shirt = Shirt()
+    shirt.add_page()
+    shirt.output("shirt.pdf")
 
 if __name__ == "__main__":
     main()
