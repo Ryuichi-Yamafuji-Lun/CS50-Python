8da6f9e0d7ceb19bea811998cd44aa9ba765c002
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-01-19 15:50:04 +0900

    Update shirtificate.py

--- shirtificate.py
+++ shirtificate.py
@@ -3,7 +3,9 @@ from fpdf import FPDF
 
 class Shirt():
     def __init__(self, name):
-        ...
+        self._pdf = FPDF()
+        self._pdf.add_page()
+        
     
 def main():
     name = input("Name: ")
